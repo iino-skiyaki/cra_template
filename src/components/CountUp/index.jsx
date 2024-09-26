246e6241dfd7e91d319171bc8d1843576d832ca0
iino-skiyaki <ten.iino@skiyaki.com>
2024-09-27 01:11:59 +0900

    Change カウントアップが行なわれた時、カウント数がアニメーションされるようにした

--- src/components/CountUp/index.jsx
+++ src/components/CountUp/index.jsx
@@ -1,15 +1,25 @@
-const { useState } = require("react");
+import { useState, useRef } from "react"
+import './index.css'
 
 const CountUp = () => {
-  const [count, setCount] = useState(0);
+  const [count, setCount] = useState(0)
+  const [isCountChanged, setIsCountChanged] = useState(false)
+  const countChangedTimerRef = useRef(null)
 
   const handleClick = () => {
     setCount(count + 1)
+    if(countChangedTimerRef.ref) {
+      clearTimeout(countChangedTimerRef.ref)
+    }
+    setIsCountChanged(true)
+    countChangedTimerRef.ref = setTimeout(() => {
+      setIsCountChanged(false)
+    }, 200)
   };
 
   return (
     <div>
-      <p>{count}</p>
+      <p className={`count ${isCountChanged?'changed' : ''}`}>{count}</p>
       <button onClick={handleClick}>Click me</button>
     </div>
   );
