a900828a2dbc6b11e539e87bfaaa89333ffda445
iino-skiyaki <ten.iino@skiyaki.com>
2024-09-27 08:18:15 +0900

    GitHub Action: Fetch PR diff and file changes for enhanced logging

--- actions_job/handle_commits.js
+++ actions_job/handle_commits.js
@@ -1,15 +1,21 @@
 const fs = require('fs');
 
-// JSONファイルを読み込む
-fs.readFile('pr_commits.json', 'utf8', (err, data) => {
+// コミット情報の処理
+fs.readFile('pr_commits.json', 'utf8', (err, commitData) => {
     if (err) {
-        console.error('Error reading the file:', err);
+        console.error('Error reading the commits file:', err);
         process.exit(1);
     }
-
-    // JSONをオブジェクトに変換
-    const commits = JSON.parse(data);
-
-    // commitsデータを使って処理を行う
+    const commits = JSON.parse(commitData);
     console.log('Commits:', commits);
+});
+
+// 変更されたファイル情報の処理
+fs.readFile('pr_files.json', 'utf8', (err, fileData) => {
+    if (err) {
+        console.error('Error reading the file changes file:', err);
+        process.exit(1);
+    }
+    const fileChanges = JSON.parse(fileData);
+    console.log('File Changes:', fileChanges);
 });
