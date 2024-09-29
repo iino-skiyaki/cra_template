b81832fd5b48f552b184d6918c3d8414da55906c
iino-skiyaki <ten.iino@skiyaki.com>
2024-09-29 19:34:02 +0900

    GitHub Action: Add video creation and upload functionality to workflow

---
+++ actions_job/create_video.js
@@ -0,0 +1,12 @@
+import { createFFmpeg, fetchFile } from '@ffmpeg/ffmpeg';
+import fs from'fs';
+
+const ffmpeg = createFFmpeg({ log: true });
+
+(async () => {
+  await ffmpeg.load();
+  ffmpeg.FS('writeFile', 'input.txt', new TextEncoder().encode('HelloWorld'));
+  await ffmpeg.run('-f', 'lavfi', '-i', 'color=c=blue:s=1280x720:d=5', '-vf', 'drawtext=fontfile=/path/to/font.ttf:textfile=input.txt:fontcolor=white:fontsize=60:x=(w-text_w)/2:y=(h-text_h)/2', 'output.mp4');
+  const data = ffmpeg.FS('readFile', 'output.mp4');
+  fs.writeFileSync('hello_world.mp4', Buffer.from(data));
+})();
