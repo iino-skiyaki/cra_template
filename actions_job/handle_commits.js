09945468ee35491a155ecf479ef1bb6065e9d6d6
iino-skiyaki <ten.iino@skiyaki.com>
2024-09-29 18:59:36 +0900

    GitHub Action: Refactor file changes handling to map file details and include patch information

--- actions_job/handle_commits.js
+++ actions_job/handle_commits.js
@@ -1,5 +1,4 @@
 const fs = require('fs');
-const { execSync } = require('child_process');
 
 // コミット情報を読み込む
 fs.readFile('pr_commits.json', 'utf8', (err, commitData) => {
@@ -18,45 +17,18 @@ fs.readFile('pr_commits.json', 'utf8', (err, commitData) => {
         }
         
         const fileChanges = JSON.parse(fileData);
-        console.log('File Changes:', JSON.stringify(fileChanges, null, 2));
-        
-        // コミット詳細の配列を作成
-        const commitDetails = commits.commits.map(commit => {
-            // コミットメッセージ
-            const message = commit.message;
+        // 変更されたファイル毎に変更内容を配列にする
+        const fileChangesMap = fileChanges.map(file => ({
+            fileName: file.filename,
+            change: {
+                status: file.status,
+                additions: file.additions,
+                deletions: file.deletions,
+                patch: file.patch // 変更内容 (パッチ情報)
+            }
+        }));
 
-            // コミッター情報
-            const author = commit.author?.name || 'Unknown';
-
-            // コミットハッシュ
-            const sha = commit.oid;
-
-            // 変更内容を抽出
-            const changes = fileChanges
-                .filter(file => file.sha === sha)
-                .map(file => ({
-                    filename: file.filename,
-                    status: file.status,
-                    additions: file.additions,
-                    deletions: file.deletions,
-                    lines: [], // 変更行を格納する配列
-                    changes: [] // 変更内容を格納する配列
-                }));
-
-            // 各ファイルに対する変更行の詳細を取得
-            changes.forEach(change => {
-                console.log('change in file content:', change);
-            });
-
-            return {
-                message: message,
-                author: author,
-                change: changes, // 変更内容をここに格納
-                sha: sha // コミットハッシュをここに格納
-            };
-        });
-
-        // ログ出力
-        console.log('Commit Details:', JSON.stringify(commitDetails, null, 2));
+        // 変更されたファイル毎の変更内容配列をログ出力
+        console.log(JSON.stringify(fileChangesMap, null, 2));
     });
 });
