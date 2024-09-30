8df4b9ac6465660cf6ecc574368f0c45c35cf365
iino-skiyaki <ten.iino@skiyaki.com>
2024-09-30 12:54:31 +0900

    GitHub Action: Refactor video creation to use ffmpeg.wasm and generate a 'HelloWorld' video

--- actions_job/create_video.js
+++ actions_job/create_video.js
@@ -1,12 +1,29 @@
-import { createFFmpeg, fetchFile } from '@ffmpeg/ffmpeg';
+const ffmpeg = require('@ffmpeg.wasm/main');
 const fs = require('fs');
 
-const ffmpeg = createFFmpeg({ log: true });
-
-(async () => {
+// Initialize ffmpeg.wasm
+const createHelloWorldVideo = async () => {
   await ffmpeg.load();
-  ffmpeg.FS('writeFile', 'input.txt', new TextEncoder().encode('HelloWorld'));
-  await ffmpeg.run('-f', 'lavfi', '-i', 'color=c=blue:s=1280x720:d=5', '-vf', 'drawtext=fontfile=/path/to/font.ttf:textfile=input.txt:fontcolor=white:fontsize=60:x=(w-text_w)/2:y=(h-text_h)/2', 'output.mp4');
-  const data = ffmpeg.FS('readFile', 'output.mp4');
-  fs.writeFileSync('hello_world.mp4', Buffer.from(data));
-})();
+
+  // Input text file for ffmpeg
+  const textContent = `
+    [0:v]
+    drawtext=text='HelloWorld':fontcolor=white:fontsize=60:x=(w-text_w)/2:y=(h-text_h)/2:duration=5
+  `;
+  
+  // Create an empty 5 second video with 'HelloWorld' text
+  ffmpeg.FS('writeFile', 'text.txt', new TextEncoder().encode(textContent));
+  
+  await ffmpeg.run(
+    '-f', 'lavfi',
+    '-i', 'color=c=blue:s=640x360:d=5',
+    '-vf', "drawtext=text='HelloWorld':fontcolor=white:fontsize=60:x=(w-text_w)/2:y=(h-text_h)/2",
+    '-t', '5',
+    'helloWorld.mp4'
+  );
+
+  const data = ffmpeg.FS('readFile', 'helloWorld.mp4');
+  fs.writeFileSync('./helloWorld.mp4', Buffer.from(data));
+};
+
+createHelloWorldVideo();
